8d4f1c837c58fee34cac17ac5e195a5cc0416988
M. Kaschub <github@mkaschub.de>
2020-05-21 21:18:51 +0200

    generate multiple ecus

--- database/generate_arxml.py
+++ database/generate_arxml.py
@@ -3,21 +3,22 @@ import arxml
 
 gen = arxml.Generator("CAN_DC_Motor")
 
-gen.add_ecu('Motor1')
 gen.add_ecu('Main')
 
-base_id = 0x100
+for i in range(1, 3):
+    ecu = f'Motor{i}'
+    base_id = 0x100 + (i - 1) * 32
+    gen.add_ecu(ecu)
 
+    gen.add_pydb_frame(ecu, 'Main', base_id + 0, 'PWM',      'EV', 'bBHH', ('Direction', 'Duty', 'none1', 'Timeout',))
+    gen.add_pydb_frame(ecu, 'Main', base_id + 1, 'Position', 'EV', 'iH', ('target_pos', 'Timeout',))
+    gen.add_pydb_frame(ecu, 'Main', base_id + 2, 'Speed',    'EV', 'iH', ('target_speed', 'Timeout',))
 
-gen.add_pydb_frame('Motor1', 'Main', base_id + 0, 'PWM',      'EV', 'bBHH', ('Direction', 'Duty', 'none1', 'Timeout',))
-gen.add_pydb_frame('Motor1', 'Main', base_id + 1, 'Position', 'EV', 'iH', ('target_pos', 'Timeout',))
-gen.add_pydb_frame('Motor1', 'Main', base_id + 2, 'Speed',    'EV', 'iH', ('target_speed', 'Timeout',))
+    #add_pydb_frame(root, ecu, 'Main', 0x10A, 'PID1',     'EV', 'ff',    ('PID_kP', 'PID_kI',))
+    # add_pydb_frame(root, ecu, 'Main', 0x10B, 'PID2',     'EV', 'ff',    ('PID_kD', 'PID_max',))
 
-#add_pydb_frame(root, 'Motor1', 'Main', 0x10A, 'PID1',     'EV', 'ff',    ('PID_kP', 'PID_kI',))
-# add_pydb_frame(root, 'Motor1', 'Main', 0x10B, 'PID2',     'EV', 'ff',    ('PID_kD', 'PID_max',))
-
-gen.add_pydb_frame('Motor1', 'Main', base_id + 8, 'Status1',  'RX', 'bBBBi', ('Direction', 'Duty', 'Mode', 'None2', 'Position',))
-gen.add_pydb_frame('Motor1', 'Main', base_id + 9, 'Status2',  'RX', 'fi',    ('VBAT', 'Target',))
+    gen.add_pydb_frame(ecu, 'Main', base_id + 8, 'Status1',  'RX', 'bBBBi', ('Direction', 'Duty', 'Mode', 'None2', 'Position',))
+    gen.add_pydb_frame(ecu, 'Main', base_id + 9, 'Status2',  'RX', 'fi',    ('VBAT', 'Target',))
 
 
 etree.ElementTree(gen.root).write("can_motor.arxml", encoding='utf-8', xml_declaration=True, pretty_print=True)
